f853941b36d8bfe1e4957fa354527dab10ea8f80
hitenzo <wmowski@gmail.com>
2017-03-08 13:01:53 +0100

    saving and deleting informations from db

---
+++ src/main/java/XmlGenerator/EmailInfo.java
@@ -0,0 +1,114 @@
+package XmlGenerator;
+
+/**
+ * Created by hitenz on 07.03.2017.
+ */
+public class EmailInfo implements java.io.Serializable {
+
+    private int id;
+    private String login;
+    private String password;
+    private String repeat_password;
+    private String name;
+    private String surname;
+    private String fullname;
+    private String city_postalcode;
+    private String alternative_contact;
+    private String birthDate_day;
+    private String birthDate_month;
+    private String birthDate_year;
+
+
+    public int getId() { return id; }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRepeat_password() {
+        return repeat_password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getCity_postalcode() {
+        return city_postalcode;
+    }
+
+    public String getAlternative_contact() {
+        return alternative_contact;
+    }
+
+    public String getBirthDate_day() {
+        return birthDate_day;
+    }
+
+    public String getBirthDate_month() {
+        return birthDate_month;
+    }
+
+    public String getBirthDate_year() {
+        return birthDate_year;
+    }
+
+    public void setId(int id) { this.id = id; }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRepeat_password(String repeat_password) {
+        this.repeat_password = repeat_password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public void setCity_postalcode(String city_postalcode) {
+        this.city_postalcode = city_postalcode;
+    }
+
+    public void setAlternative_contact(String alternative_contact) {
+        this.alternative_contact = alternative_contact;
+    }
+
+    public void setBirthDate_day(String birthDate_day) {
+        this.birthDate_day = birthDate_day;
+    }
+
+    public void setBirthDate_month(String birthDate_month) {
+        this.birthDate_month = birthDate_month;
+    }
+
+    public void setBirthDate_year(String birthDate_year) {
+        this.birthDate_year = birthDate_year;
+    }
+}
+
---
+++ src/main/java/XmlGenerator/EmailsManager.java
@@ -0,0 +1,89 @@
+package XmlGenerator;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by hitenz on 07.03.2017.
+ */
+public class EmailsManager {
+
+    private ArrayList<EmailInfo> emailsList = new ArrayList<EmailInfo>();
+    private SessionFactory sessionFactory;
+    private Session session;
+
+    public EmailsManager(){
+        Configuration conf = new Configuration(); //"hibernate.cfg.xml"
+        conf.addResource("EmailInfo.hbm.xml");
+        conf.configure();
+        ServiceRegistry sessionRegistry = new StandardServiceRegistryBuilder().applySettings(conf.getProperties()).build();
+        sessionFactory = conf.buildSessionFactory(sessionRegistry);
+        session = sessionFactory.openSession();
+    }
+
+    public void createEmails(int counter){
+
+        for(int i=0; i<counter; i++){
+            EmailInfo email = new EmailInfo();
+            email.setLogin(getLogin());
+            email.setPassword("dupa123DUPA987");
+            email.setRepeat_password("dupa123DUPA987");
+            email.setName("Jan");
+            email.setSurname("Kowalski");
+            email.setFullname("Jan Kowalski");
+            email.setCity_postalcode("Warszawa 02-153");
+            email.setAlternative_contact("bourn2@wp.pl");
+            email.setBirthDate_day("13");
+            email.setBirthDate_month("marca");
+            email.setBirthDate_year("1983");
+            emailsList.add(email);
+        }
+    }
+
+    public void saveEmailsToDB(){
+
+        session.beginTransaction();
+
+        for(EmailInfo email : emailsList){
+            session.save(email);
+        }
+
+        session.getTransaction().commit();
+    }
+
+    public void deleteEmailsFromDB(int id){
+        session.beginTransaction();
+        EmailInfo email = (EmailInfo)session.load(EmailInfo.class, id);
+        session.delete(email);
+        session.getTransaction().commit();
+    }
+
+    public void closeDBConnection(){
+        session.close();
+        sessionFactory.close();
+    }
+
+    private static String getLogin(){
+        String login = "";
+        Random rand = new Random();
+
+        for(int i=0; i<8; i++){
+            char randomCharacter = (char)(rand.nextInt(26) + 'a');
+            login = login.concat(Character.toString(randomCharacter));
+        }
+
+        for(int j=0; j<5; j++){
+            int randomNumber = rand.nextInt(10);
+            login = login.concat(Integer.toString(randomNumber));
+        }
+
+        return login;
+    }
+}
+
